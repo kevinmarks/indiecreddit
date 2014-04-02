6d8ab6053bc02401eaa793d6bff08eb03ae14938
Kevin Marks <kevinmarks@gmail.com>
2014-04-02 02:04:53 -0700

    lower reward threshold so we get some coins

--- src/main.cpp
+++ src/main.cpp
@@ -831,7 +831,7 @@ int64 static GetBlockValue(int nHeight, int64 nFees)
     int64 nSubsidy = 4 * COIN;
 
 
-    if(nHeight < 17280) // no block reward within the first 3 days
+    if(nHeight < 80) // no block reward within the first  day
         nSubsidy = 0;
     if(nHeight > 10519200) // no block reward after 5 years
         nSubsidy = 0;
